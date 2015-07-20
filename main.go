ca44853347d8a5a91138bd58ec4e677fb75d1e9b
Will Barrett <will@tallgreentree.com>
2015-07-20 15:26:40 -0400

    Code cleanup

--- main.go
+++ main.go
@@ -9,35 +9,56 @@ import (
 	"github.com/getsentry/raven-go"
 )
 
-func main() {
-	var hostname = flag.String("hostname", os.Getenv("HOSTNAME"), "The server name to report to Sentry")
-	var sentry_dsn = flag.String("sentry-dsn", os.Getenv("SENTRY_DSN"), "The Sentry DSN to use for the error report")
-	var app_environment = flag.String("app_environment", os.Getenv("APP_ENVIRONMENT"), "The environment to report for, e.g. staging, production")
-	var report = flag.String("err", os.Getenv("APP_ERROR"), "The error to report")
+var (
+	hostname        string
+	sentry_dsn      string
+	app_environment string
+	report          string
+)
 
-	if *hostname == "" {
+func main() {
+	// Set up the flags for the command
+	flag.StringVar(&hostname, "hostname", os.Getenv("HOSTNAME"), "The server name to report to Sentry")
+	flag.StringVar(&sentry_dsn, "sentry-dsn", os.Getenv("SENTRY_DSN"), "The Sentry DSN to use for the error report")
+	flag.StringVar(&app_environment, "app_environment", os.Getenv("APP_ENVIRONMENT"), "The environment to report for, e.g. staging, production")
+	flag.StringVar(&report, "report", os.Getenv("REPORT"), "The error to report")
+
+	// Parse the command line flags
+	flag.Parse()
+
+	// Make sure we've got everything
+	if hostname == "" {
 		log.Fatal("Hostname is not configured")
 	}
-	if *sentry_dsn == "" {
+	if sentry_dsn == "" {
 		log.Fatal("Sentry DSN not configured")
 	}
-	if *app_environment == "" {
+	if app_environment == "" {
 		log.Fatal("You must set the app environment")
 	}
-	if *report == "" {
+	if report == "" {
 		log.Fatal("You must pass an error to report")
 	}
 
-	ravenClient, err := raven.NewClient(*sentry_dsn, map[string]string{"environment": *app_environment, "server_name": *hostname})
+	// Create the Raven Client
+	ravenClient, err := raven.NewClient(sentry_dsn, map[string]string{"environment": app_environment, "server_name": hostname})
 	if err != nil {
 		log.Fatal(err)
 	}
-	packet := raven.NewPacket(*report, raven.NewException(errors.New(*report), raven.NewStacktrace(0, 5, nil)))
+
+	// Set up the packet to send
+	packet := raven.NewPacket(report, raven.NewException(errors.New(report), raven.NewStacktrace(0, 5, nil)))
+
+	// Send the packet
 	_, errch := ravenClient.Capture(packet, nil)
+
+	// Handle Sentry response
 	if err := <-errch; err != nil {
 		log.Fatalf("Error sending to Raven: %v", err)
 	} else {
 		log.Printf("Sent error to Sentry: %v", err)
 	}
+
+	// Quit
 	os.Exit(0)
 }
